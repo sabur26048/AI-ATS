89b0e5bf00ac7ab002cf39810512cf6f442fc6be
sabur ali <sabur.ali@codeclouds.in>
2025-02-21 12:25:46 +0530

    Initial commmit

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/AI-ATS.IO', indexRouter);
+app.use('/', indexRouter);
 
 
 app.use(function(req, res, next) {
